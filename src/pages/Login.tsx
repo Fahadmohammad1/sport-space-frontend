bd86ca27e01cc1d69c298394ad4f3207d15f48e1
Fahad <205fahadmohammad@gmail.com>
2024-09-12 23:09:55 +0600

    login page updated

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -10,22 +10,36 @@ const Login = () => {
           className="object-cover object-center h-screen w-1/2"
         />
         <div className="bg-white flex flex-col justify-center items-center w-1/2 shadow-lg">
-          <h1 className="text-3xl font-bold text-orange-500 mb-2">LOGIN</h1>
+          <h1 className="text-3xl font-bold text-ss-primary mb-2">LOGIN</h1>
           <div className="w-1/2 text-center">
-            <input
-              type="text"
-              name="username"
-              placeholder="username"
-              autoComplete="off"
-              className="shadow-md border w-full h-10 px-3 py-2 text-orange-500 focus:outline-none focus:border-orange-500 mb-3 rounded"
-            />
-            <input
-              type="password"
-              name="password"
-              placeholder="password"
-              autocomplete="off"
-              className="shadow-md border w-full h-10 px-3 py-2 text-orange-500 focus:outline-none focus:border-orange-500 mb-3 rounded"
-            />
+            <label className="input input-bordered flex items-center gap-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                className="h-4 w-4 opacity-70"
+              >
+                <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
+                <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
+              </svg>
+              <input type="text" className="grow" placeholder="Email" />
+            </label>
+
+            <label className="input input-bordered flex items-center gap-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                className="h-4 w-4 opacity-70"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                  clipRule="evenodd"
+                />
+              </svg>
+              <input type="password" className="grow" value="password" />
+            </label>
             <button className="bg-orange-500 hover:bg-orange-600 text-white px-3 py-1 rounded text-lg focus:outline-none shadow">
               Sign In
             </button>
